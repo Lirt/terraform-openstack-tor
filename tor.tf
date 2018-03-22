b14574a5d9ed1d82b7d7d1a0f8ce329ab64d742a
Ondrej Vasko <ondrej.vaskoo@gmail.com>
2018-03-22 11:57:25 +0100

    Parametrize static parameters and move variables to `variables.tf`
    Signed-off-by: Ondrej Vasko <ondrej.vaskoo@gmail.com>

--- tor.tf
+++ tor.tf
@@ -1,14 +1,3 @@
-variable TOR_OS_NETWORK_NAME {}
-variable TOR_OS_FLAVOR_NAME {}
-variable TOR_OS_IMAGE_NAME {}
-
-variable SSH_PUB_KEY_LOCATION {}
-variable SSH_PRIV_KEY_LOCATION {}
-variable TOR_RELAY_NODE_COUNT {}
-
-variable ANSIBLE_RELAYOR_PLAYBOOK_PATH {}
-variable ANSIBLE_PLAYBOOK_USER {}
-
 provider "openstack" {
 }
 
@@ -25,12 +14,12 @@ data "openstack_networking_network_v2" "public_net" {
 
 
 resource "openstack_compute_keypair_v2" "tor_keypair" {
-  name       = "vnet-tor-cluster-keypair"
+  name       = "${var.TOR_OS_KEYPAIR_NAME}"
   public_key = "${file(var.SSH_PUB_KEY_LOCATION)}"
 }
 
 resource "openstack_compute_secgroup_v2" "ssh_secgroup" {
-    name = "allow-ssh-all-ip"
+    name = "tor-allow-ssh-all-ip"
     description = "Allow SSH from all IP addresses"
     rule {
         ip_protocol = "tcp"
@@ -40,8 +29,8 @@ resource "openstack_compute_secgroup_v2" "ssh_secgroup" {
     }
 }
 resource "openstack_compute_secgroup_v2" "tor_or_1_secgroup" {
-    name = "allow-tor-or-1-all"
-    description = "Allow OR port 1"
+    name = "tor-allow-or-1-all"
+    description = "Allow Tor OR port 1"
     rule {
         ip_protocol = "tcp"
         from_port = "9000"
@@ -50,8 +39,8 @@ resource "openstack_compute_secgroup_v2" "tor_or_1_secgroup" {
     }
 }
 resource "openstack_compute_secgroup_v2" "tor_or_2_secgroup" {
-    name = "allow-tor-or-2-all"
-    description = "Allow OR port 2 "
+    name = "tor-allow-or-2-all"
+    description = "Allow Tor OR port 2 "
     rule {
         ip_protocol = "tcp"
         from_port = "9100"
@@ -60,8 +49,8 @@ resource "openstack_compute_secgroup_v2" "tor_or_2_secgroup" {
     }
 }
 resource "openstack_compute_secgroup_v2" "tor_dir_1_secgroup" {
-    name = "allow-tor-dir-1-all"
-    description = "Allow Dir port 1 "
+    name = "tor-allow-dir-1-all"
+    description = "Allow Tor Directory port 1 "
     rule {
         ip_protocol = "tcp"
         from_port = "9001"
@@ -70,8 +59,8 @@ resource "openstack_compute_secgroup_v2" "tor_dir_1_secgroup" {
     }
 }
 resource "openstack_compute_secgroup_v2" "tor_dir_2_secgroup" {
-    name = "allow-tor-dir-2-all"
-    description = "Allow Dir port 2 "
+    name = "tor-allow-dir-2-all"
+    description = "Allow Tor Directory port 2 "
     rule {
         ip_protocol = "tcp"
         from_port = "9101"
@@ -83,7 +72,7 @@ resource "openstack_compute_secgroup_v2" "tor_dir_2_secgroup" {
 
 resource "openstack_compute_instance_v2" "tor_relay_nodes" {
   count             = "${var.TOR_RELAY_NODE_COUNT}"
-  name              = "${format("vnet-tor-relay-node-%02d", count.index + 1)}"
+  name              = "${format("${var.TOR_RELAY_NICKNAME}-%02d", count.index + 1)}"
   image_id          = "${data.openstack_images_image_v2.image.id}"
   flavor_name       = "${var.TOR_OS_FLAVOR_NAME}"
   key_pair          = "${openstack_compute_keypair_v2.tor_keypair.id}"
@@ -92,7 +81,7 @@ resource "openstack_compute_instance_v2" "tor_relay_nodes" {
                        "${openstack_compute_secgroup_v2.tor_or_2_secgroup.name}",
                        "${openstack_compute_secgroup_v2.tor_dir_1_secgroup.name}",
                        "${openstack_compute_secgroup_v2.tor_dir_2_secgroup.name}",
-                        "default"
+                       "default"
   ]
   network           = {
     name = "${data.openstack_networking_network_v2.public_net.name}"
@@ -101,7 +90,7 @@ resource "openstack_compute_instance_v2" "tor_relay_nodes" {
 
 resource "openstack_networking_floatingip_v2" "tor_relay_fips" {
   count = "${var.TOR_RELAY_NODE_COUNT}"
-  pool  = "ext-net"
+  pool  = "${var.EXT_NET_POOL}"
 }
 
 resource "openstack_compute_floatingip_associate_v2" "tor_relay_assoc_fips" {
@@ -110,11 +99,6 @@ resource "openstack_compute_floatingip_associate_v2" "tor_relay_assoc_fips" {
   instance_id = "${element(openstack_compute_instance_v2.tor_relay_nodes.*.id, count.index)}"
 
   provisioner "local-exec" {
-    command    = "echo '${element(openstack_networking_floatingip_v2.tor_relay_fips.*.address, count.index)}' >> ansible_inventory"
-    on_failure = "fail"
-  }
-
-  provisioner "local-exec" {
-    command = "sleep 200; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ANSIBLE_PLAYBOOK_USER} --private-key ${var.SSH_PRIV_KEY_LOCATION} -i '${element(openstack_networking_floatingip_v2.tor_relay_fips.*.address, count.index)},' ${var.ANSIBLE_RELAYOR_PLAYBOOK_PATH}"
+    command = "sleep 200; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ANSIBLE_PLAYBOOK_USER} --private-key ${var.SSH_PRIV_KEY_LOCATION} -i '${element(openstack_networking_floatingip_v2.tor_relay_fips.*.address, count.index)},' ${var.ANSIBLE_RELAYOR_RELAY_PLAYBOOK_PATH}"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "TOR_OS_NETWORK_NAME" {
+  default = "public-net"
+}
+variable "TOR_OS_FLAVOR_NAME" {
+  default = "t2.small"
+}
+variable "TOR_OS_IMAGE_NAME" {
+  default = "ubuntu-16.04"
+}
+variable "TOR_OS_EXT_NET_POOL" {
+  default = "ext-net"
+}
+variable "TOR_OS_KEYPAIR_NAME" {
+  default = "tor-tf-ssh-key"
+}
+
+variable "SSH_PUB_KEY_LOCATION" {
+  default = "./tor-ssh-key.pub"
+}
+variable "SSH_PRIV_KEY_LOCATION" {
+  default = "./tor-ssh-key.rsa"
+}
+
+variable "TOR_RELAY_NODE_COUNT" {
+  default = 1
+}
+variable "TOR_RELAY_NICKNAME" {
+  default = "tor-tf-relay"
+}
+variable "TOR_EXIT_NODE_COUNT" {
+  default = 0
+}
+variable "TOR_EXIT_NICKNAME" {
+  default = "tor-tf-exit"
+}
+
+variable "ANSIBLE_RELAYOR_RELAY_PLAYBOOK_PATH" {
+  default = "playbooks/relay-node.yml"
+}
+variable "ANSIBLE_PLAYBOOK_USER" {
+  default = "ubuntu"
+}
